e31893fb8e28a952148cb27ebc3f0ac9fafa0f81
silvaxxx1 <silvapi1994@gmail.com>
2025-03-09 04:17:08 +0300

    addiing to llama 2 and 3

--- MyLLM/myllm/model/llama2.py
+++ MyLLM/myllm/model/llama2.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 
 import sentencepiece 
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 LLAMA2_CONFIG_7B = {
     "vocab_size": 32000,     # Vocabulary size
     "context_length": 4096,  # Context length
@@ -16,44 +18,62 @@ LLAMA2_CONFIG_7B = {
 
 class llama2(nn.Module):
     def __init__(self, config):
-        pass 
+        self.token_emb = nn.Embedding(config["vocab_size"], config["emb_dim"],dtype=config["dtype"]) 
+        self.trs_blk = nn.ModuleList(Transformer(config) for _ in range(config['n_layer'])) 
+        self.norm = nn.LayerNorm(config["emb_dim"]) 
+        self.proj = nn.Linear(config['emb_dim'], config['vocab_size'], dtype=config["dtype"] , bias=False)
 
     def forward(self , x):
-        pass 
+        x = self.token_emb(x) 
+        x = self.trs_blk(x) 
+        x = self.norm(x)
+        logits = self.proj(x)
+
+        return logits
+
 
 
 class Transformer(nn.Module):
     def __init__(self,config):
-        pass
+        self.norm1 = nn.RMSNorm(config["emb_dim"], eps=1e-5)
+        self.norm2 = nn.RMSNorm(config["emb_dim"], eps=1e-5)
+        self.atten = FlashAttention(config) 
+        
 
 
     def forward(self, x):
-        pass
+        shortcut = x
+        x = self.norm1(x)
+        x = self.atten(x)
+        x = self.norm2(x)
+        x = shortcut + x
 
+        return x
 
-class RMSNorm(nn.Module):
-    def __init__(self, config): 
-        pass 
-
-    def forward(self, x):
-        pass 
 
 
 class FlashAttention(nn.Module):
     def __init__(self, config):
-        pass 
+        pass
 
 
     def forward(self , x):
         pass
 
 
+
 class FeedForward(nn.Module):
-    def __init__(self, config):
-        pass
-    
+    def __init__(self, cfg):
+        super().__init__()
+        self.fc1 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc2 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc3 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+
     def forward(self, x):
-        pass 
+        x_fc1 = self.fc1(x)
+        x_fc2 = self.fc2(x)
+        x = nn.functional.silu(x_fc1) * x_fc2
+        return self.fc3(x)
 
 
 
--- MyLLM/myllm/model/llama3.py
+++ MyLLM/myllm/model/llama3.py
@@ -15,29 +15,67 @@ LLAMA3_CONFIG_1B = {
     "dtype": torch.bfloat16  # NEW: Lower-precision dtype to reduce memory usage
 }
 
-class llama3(nn.Module):
+class Llama3(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"], dtype=cfg["dtype"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        self.final_norm = nn.RMSNorm(cfg["emb_dim"], eps=1e-5)
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False, dtype=cfg["dtype"])
+
+    def forward(self, in_idx):
+        tok_embeds = self.tok_emb(in_idx)
+        x = tok_embeds
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x.to(torch.bfloat16))
+        return logits
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att =  GroupedQueryFlashAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            num_kv_groups=cfg["n_kv_groups"],
+            rope_base=cfg["rope_base"],
+            rope_config=cfg["rope_freq"],
+            dtype=cfg["dtype"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = nn.RMSNorm(cfg["emb_dim"], eps=1e-5)
+        self.norm2 = nn.RMSNorm(cfg["emb_dim"], eps=1e-5)
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x.to(torch.bfloat16))   # Shape [batch_size, num_tokens, emb_size]
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed-forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x.to(torch.bfloat16))
+        x = x + shortcut  # Add the original input back
+
+        return x
+
+class GroupedQueryFlashAttention(nn.Module):
     def __init__(self, config):
         pass 
 
     def forward(self , x):
-        pass 
-
-
-class Transformer(nn.Module):
-    def __init__(self,config):
         pass
 
 
-    def forward(self, x):
-        pass
-
-
-class RMSNorm(nn.Module):
-    def __init__(self, config): 
-        pass 
-
-    def forward(self, x):
-        pass 
+    
 
 
 class FlashAttention(nn.Module):
@@ -49,15 +87,25 @@ class FlashAttention(nn.Module):
         pass
 
 
+
 class FeedForward(nn.Module):
-    def __init__(self, config):
-        pass
-    
+    def __init__(self, cfg):
+        super().__init__()
+        self.fc1 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc2 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
+        self.fc3 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+
     def forward(self, x):
-        pass 
+        x_fc1 = self.fc1(x)
+        x_fc2 = self.fc2(x)
+        x = nn.functional.silu(x_fc1) * x_fc2
+        return self.fc3(x)
 
 
 
 
     
+
+
+
 
