fc850fb92ee20dde3f4a4af3276ef939ac7e4147
silvaxxx1 <silvapi1994@gmail.com>
2025-04-23 03:32:38 +0300

    start api.py

---
+++ MyLLM/api.py
@@ -0,0 +1,4 @@
+# api wrapper around the gpt class form the model.py file 
+# thius will allowed to use the gpt class in a more user friendly way 
+# to load the model open weights and use it for inference 
+# or use it for training and fine tuning the model 
